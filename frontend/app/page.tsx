aa73af2d9f288253a26577e3e2f1fe7d753dabc8
itsevelync <evelynchen555@gmail.com>
2025-03-20 21:20:56 -0400

    Initial landing page commit + global style updates

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,101 +1,82 @@
 import Image from "next/image";
+import Link from 'next/link'
+import { FaSearch, FaChevronDown } from "react-icons/fa";
+import { TbSend2 } from "react-icons/tb";
+import mockCalls from "@/public/data/mock-calls.json";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+  return (
+    <div className="bg-[url(/bg.svg)] bg-cover grid grid-rows-[20px_1fr_20px] justify-items-center p-8 md:p-12 gap-16 font-[family-name:var(--font-montserrat)]">
+      <main className="text-center flex flex-col gap-8 row-start-2 items-center max-w-[1100px]">
+        <div className="flex flex-col items-center w-full">
+          <hr className="w-full h-1 border-none bg-gradient-to-r from-[rgba(129,209,141,0)] via-[rgba(129,209,141,0.15)] to-[rgba(129,209,141,0)]" />
+          <div className="text-green -mt-4.5 px-4 py-1 rounded-full bg-[#131B15] shadow-[0px_0px_6.414px_0px_rgba(129,209,141,0.56)]">Investing. Made simple.</div>
+        </div>
+        <h1 className='text-5xl md:text-6xl/20 font-medium mt-4'>Turning Earnings Calls Into <span className="font-semibold bg-linear-to-r from-green to-[#426B48] bg-clip-text text-transparent">Actionable Insights</span></h1>
+        <h4 className="text-xl mb-4">Our AI-powered platform <u>simplifies earnings calls</u>, providing easy-to-understand summaries, sentiment analysis, and actionable insights, helping you make informed financial decisions.</h4>
+        <div className="w-full text-left justify-items-center rounded-md p-7 bg-[rgba(0, 0, 0, 0.15)] shadow-[0px_0px_7.258px_0px_rgba(129,209,141,0.50)]">
+          <h4 className="font-semibold text-lg mb-3 w-full">Upload an <span className='text-green'>earnings call</span> to get started:</h4>
+          <div className="w-full rounded-full bg-[rgba(234, 250, 236, 0.14)] flex items-center">
+            <FaSearch className='ml-3 text-[#808280]' />
+            <input className="-ml-7 py-2 px-10 bg-[rgba(234,250,236,0.14)] rounded-full inset-shadow-sm inset-shadow-[rgba(0,0,0,0.25)] w-full" placeholder="Paste earnings call link here...." />
+            <a className="-ml-11" href="#">
+              <div className="transition-all bg-green rounded-full px-2 py-1 hover:brightness-80"><TbSend2 size={20} className='text-[#2D322E]' /></div>
+            </a>
+          </div>
+          <a href="#earnings-calls">
+            <p className="transition-all flex items-center gap-4 mt-4 opacity-75 hover:opacity-60">or choose from our library below <FaChevronDown /></p>
           </a>
         </div>
+        <hr id="earnings-calls" className="w-full my-7 h-1 border-none bg-[linear-gradient(90deg,_rgba(129,209,141,0)_0%,_rgba(129,209,141,0.15)_50%,_rgba(129,209,141,0)_100%)]" />
+        <div className="grid grid-cols-[1fr_500px_1fr] items-center gap-4">
+          <div>
+            <Image
+              src="left_squares.svg"
+              alt=""
+              width={260}
+              height={150}
+              className="hidden lg:block"
+            />
+          </div>
+          <div>
+            <h3 className="text-4xl font-bold bg-linear-to-r from-green to-[#426B48] bg-clip-text text-transparent">Earnings Call Library</h3>
+            <h5 className="text-xl">Try a call from our demo library.</h5>
+          </div>
+          <div>
+            <Image
+              src="right_squares.svg"
+              alt=""
+              width={260}
+              height={150}
+              className="hidden lg:block"
+            />
+          </div>
+        </div>
+        <div className="rounded-lg px-6 pt-6 pb-4 border border-[rgba(129,209,141,0.26)] bg-[rgba(0,0,0,0.08)] shadow-[0px_0px_8px_0px_rgba(129,209,141,0.25)]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-[1000px]">
+            {mockCalls.map((call) => {
+              const formattedDate = new Date(call.date).toLocaleDateString('en-US');
+              return (
+                <Link key={call.id} href={`/dashboard/${call.id}`}>
+                  <div className="transition-all rounded-lg text-left p-3 border border-[rgba(228,243,230,0.25)] bg-[rgba(249,250,249,0.04)] shadow-[0px_0px_4px_0px_rgba(129,209,141,0.50)] hover:-translate-y-1">
+                    <Image
+                      src={call.image}
+                      alt={`${call.company} Thumbnail`}
+                      width={350}
+                      height={150}
+                      className="rounded-md"
+                    />
+                    <h4 className="text-lg font-bold mt-3">{call.symbol} (Q{call.quarter} {new Date(call.date).getFullYear()})</h4>
+                    <p className="text-sm text-gray-400">{formattedDate}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+          <p className="w-full opacity-60 mt-4"><em>Click a video card above.</em></p>
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
