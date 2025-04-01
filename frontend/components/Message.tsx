460822393deff966aecc71f3e70bdb2eba27501c
itsevelync <evelynchen555@gmail.com>
2025-04-01 15:43:05 -0400

    Update Message.tsx

--- frontend/components/Message.tsx
+++ frontend/components/Message.tsx
@@ -7,7 +7,7 @@ export default function Message({ sender, text }: MessageProps) {
     if (sender === 'user') {
         return (
             <div className="w-full flex justify-end mt-5">
-                <div className="whitespace-pre-line px-3 py-2 bg-white/4 text-white rounded-2xl border-[0.85px] border-white/25">{text}</div>
+                <div className="whitespace-pre-line max-w-[80%] px-3 py-2 bg-white/4 text-white rounded-t-2xl rounded-bl-2xl border-[0.85px] border-white/25">{text}</div>
             </div>
         );
     }
