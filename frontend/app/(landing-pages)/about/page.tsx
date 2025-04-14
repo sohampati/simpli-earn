aafd544d67913daee7fd50e6e09d32d76c4ffc48
Apramey006 <97631030+Apramey006@users.noreply.github.com>
2025-04-14 19:48:04 -0400

    Standardized Roles and Descriptions

--- frontend/app/(landing-pages)/about/page.tsx
+++ frontend/app/(landing-pages)/about/page.tsx
@@ -12,7 +12,7 @@ const teamMembers = [
   {
     name: "Parth Parikh",
     role: "Project Lead",
-    description: "I'm a first-year student studying Computer Science with concentrations in intelligence and theory. I'm passionate about real-world AI/ML applications and interested in delving deeper into academic research. In my free time, you can find me pursuing my passion for photography at parks and in nature!",
+    description: "I'm a first-year student studying Computer Science with concentrations in intelligence and theory. I'm passionate about real-world AI/ML applications and I'm interested in delving deeper into academic research. In my free time, I enjoy pursuing photography in parks and nature.",
     image: "/images/team_imgs/Parth_Parikh.jpg",
     socials: {
       linkedin: "https://www.linkedin.com/in/parthparikh04",
@@ -22,8 +22,8 @@ const teamMembers = [
   {
     name: "Vidyut Rajagopal",
     role: "Project Lead",
-    description: "Vidyut is a freshman at Georgia Tech and the co-founder of SimpliEarn. He has a strong interest in cybersecurity, cloud computing, and software development. Vidyut is passionate about applying technology to real-world challenges—especially in finance, leadership, and client-facing domains. Outside of his work with SimpliEarn, he enjoys producing music, publishing articles, and playing cricket.",
-    image: "/images/team_imgs/Vidyut_Raj.jpg",
+    description: "I'm a freshman at Georgia Tech and the co-founder of SimpliEarn. I have a strong interest in cybersecurity, cloud computing, and software development. I'm passionate about applying technology to real-world challenges—especially in finance, leadership, and client-facing domains. Outside of SimpliEarn, I enjoy producing music, publishing articles, and playing cricket.",
+    image: "/images/team_imgs/Vidyut_Raj_1.png",
     socials: {
       linkedin: "https://www.linkedin.com/in/vidyut-rajagopal",
       github: "https://github.com/vidyutraj",
@@ -32,7 +32,7 @@ const teamMembers = [
   {
     name: "Ritwij Ghosh",
     role: "Project Lead",
-    description: "Ritwij is a CS major from São Paulo, Brazil with a keen interest in applying machine learning and AI to finance and business use cases.",
+    description: "I'm a CS major from São Paulo, Brazil with a keen interest in applying machine learning and AI to finance and business use cases.",
     image: "/images/team_imgs/Ritwij_Ghosh.jpeg",
     socials: {
       linkedin: "http://www.linkedin.com/in/ritwij-ghosh",
@@ -42,7 +42,7 @@ const teamMembers = [
   {
     name: "Evelyn Chen",
     role: "Data Visualization",
-    description: "Hello hello! I'm Evelyn (she/her), and I'm from Portland, OR. I love to learn new things and enjoy using my creativity and technical programming experience to build full-stack applications. Aside from computer science, I love rock climbing, all thing outdoors (hiking, catching sunrises/sunsets, running), crafting (birthday cards are so fun!), and taking too many pics on my digital camera.",
+    description: "I'm Evelyn (she/her), from Portland, OR. I love learning new things and enjoy using my creativity and technical programming skills to build full-stack applications. I’m also into rock climbing, hiking, catching sunrises/sunsets, running, crafting (birthday cards are so fun!), and taking too many pics on my digital camera.",
     image: "/images/team_imgs/Evelyn_Chen.jpg",
     socials: {
       linkedin: "https://www.linkedin.com/in/evelynchen5/",
@@ -52,7 +52,7 @@ const teamMembers = [
   {
     name: "Neil Samant",
     role: "RAG",
-    description: "Hey! I'm Neil Samant, a mathematics major at Georgia Tech with a concentration in data science and a minor in computer science. I'm passionate about using tech to solve real-world problems, whether that's through building full-stack web apps, diving into machine learning projects, or analyzing data to uncover meaningful insights. I've worked with tools like Python, Java, JavaScript, SQL, and frameworks like React, Flask, and FastAPI, and I'm always looking to learn something new. Outside of tech, I'm a National Master in chess and have been competing for over 10 years. I love thinking strategically, whether it's on the board or in code.",
+    description: "I'm a mathematics major at Georgia Tech with a concentration in data science and a minor in computer science. I'm passionate about using tech to solve real-world problems—whether that's through building full-stack web apps, diving into machine learning projects, or analyzing data. I'm also a National Master in chess and have been competing for over 10 years.",
     image: "/images/team_imgs/Neil_Samant.jpeg",
     socials: {
       linkedin: "https://www.linkedin.com/in/neil-samant",
@@ -62,7 +62,7 @@ const teamMembers = [
   {
     name: "Kate Li",
     role: "Data Visualization",
-    description: "Hi! I'm Kate Li, a computer science major at Georgia Tech with a minor in Linguistics. I love to explore and learn new things, especially when I can use technology to solve real-world problems or chase whatever idea has currently hijacked my brain. I also am in Film Club and a big foodie, so if you have any recommendations for good restaurants or movies, let me know!",
+    description: "I'm a computer science major at Georgia Tech with a minor in Linguistics. I love exploring and learning new things, especially when I can use technology to solve real-world problems or chase whatever idea has currently hijacked my brain. I'm also in Film Club and a big foodie—let me know if you have movie or restaurant recommendations!",
     image: "/images/team_imgs/Kate_Li.png",
     socials: {
       linkedin: "https://www.linkedin.com/in/li-kate",
@@ -71,8 +71,8 @@ const teamMembers = [
   },
   {
     name: "Apramey Akkiraju",
-    role: "Front-end Development",
-    description: "Apramey is a computer science student passionate about fin-tech and the financial services domain as a whole. He joined Simpli-Earn because the mission of making investing a more accessible practice by simplifying a relatively unknown aspect of company information resonated with him.",
+    role: "Frontend",
+    description: "I'm a computer science student passionate about fintech and the financial services industry. I joined SimpliEarn because its mission of making investing more accessible through earnings call analysis really resonated with me.",
     image: "/images/team_imgs/Apramey_Akkiraju.jpeg",
     socials: {
       linkedin: "https://www.linkedin.com/in/aprameyakkiraju/",
@@ -81,8 +81,8 @@ const teamMembers = [
   },
   {
     name: "Soham Pati",
-    role: "Software Engineer – RAG",
-    description: "Soham Pati is a current second year Computer Science student @ Georgia Tech. He loves to golf and play the violin in his free time.",
+    role: "RAG",
+    description: "I'm a second-year Computer Science student at Georgia Tech. In my free time, I enjoy golfing and playing the violin.",
     image: "/images/team_imgs/Soham_Pati.jpeg",
     socials: {
       linkedin: "https://www.linkedin.com/in/soham--pati/",
@@ -91,8 +91,8 @@ const teamMembers = [
   },
   {
     name: "Soungmin (Min) Lee",
-    role: "Pipeline/Model – Sentiment Analysis",
-    description: "I'm a BS/MS student majoring Computer Science at Georgia Tech. I specialize in NLP and full-stack development, and enthusiastic in building software solutions to solve real-world problems.",
+    role: "Sentiment Analysis",
+    description: "I'm a BS/MS student majoring in Computer Science at Georgia Tech. I specialize in NLP and full-stack development, and I'm enthusiastic about building software solutions that solve real-world problems.",
     image: "/images/team_imgs/Soungmin_Lee.png",
     socials: {
       linkedin: "https://www.linkedin.com/in/soung-min-lee/",
@@ -102,7 +102,7 @@ const teamMembers = [
   {
     name: "Ethan Hu",
     role: "Sentiment Analysis",
-    description: "First-year computer science student from Greater Boston area. Interested in numbers. Enjoys spending time outdoors and baking desserts.",
+    description: "I'm a first-year computer science student from the Greater Boston area. I'm interested in numbers, and I enjoy spending time outdoors and baking desserts.",
     image: "/images/team_imgs/Ethan_Hu.jpeg",
     socials: {
       linkedin: "https://www.linkedin.com/in/ethanhu89/",
@@ -111,8 +111,8 @@ const teamMembers = [
   },
   {
     name: "Gauri Sharma",
-    role: "Sentiment Analysis Team",
-    description: "I'm a Computer Science student at Georgia Tech with an interest in Natural Language Processing and finance!",
+    role: "Sentiment Analysis",
+    description: "I'm a Computer Science student at Georgia Tech with a strong interest in Natural Language Processing and finance.",
     image: "/images/team_imgs/Gauri_Sharma.png",
     socials: {
       linkedin: "https://www.linkedin.com/in/gs-softwaredev/",
@@ -121,8 +121,8 @@ const teamMembers = [
   },
   {
     name: "Zechariah Frierson",
-    role: "Data Viz & Frontend",
-    description: "Zechariah is a freshman at Georgia Tech and a member of the data visualization team for SimpliEarn. He loves frontend web development, web design, and coding in general. Outside of programming, Zechariah enjoys playing soccer and volleyball, and spending time with friends. He is excited to work with such a talented team on a project that can provide value to anyone!",
+    role: "Frontend",
+    description: "I'm a freshman at Georgia Tech and part of the data visualization team for SimpliEarn. I love frontend web development, web design, and coding in general. Outside of programming, I enjoy playing soccer and volleyball and spending time with friends.",
     image: "/images/team_imgs/Zechariah_Frierson.jpg",
     socials: {
       linkedin: "https://www.linkedin.com/in/zechariah-frierson/",
